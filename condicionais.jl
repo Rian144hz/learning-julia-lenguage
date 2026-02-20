24e9200850117401c5affd005dc056cfae97a5a7
rian144hz <rianmatheus669@gmail.com>
2026-02-20 14:27:05 -0300

    condicionais

---
+++ condicionais.jl
@@ -0,0 +1,8 @@
+print("Qual a sua idade:")
+idade_str = readline()
+idade = parse(Int,idade_str)
+if idade_str <"18"
+    println("Você é menor de idade 👶🏾")
+    else
+    println("Você é maior e idade 👨🏾")
+end
